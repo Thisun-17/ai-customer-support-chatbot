848a3ef900ae374c26760f019d5b40846eec7d61
Thisun-17 <thisun.20233173@iit.ac.lk>
2025-04-11 21:04:54 +0530

    feat: add initial App component and index file for rendering

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>AI Customer Support Chatbot</h1>
+        <p>Welcome to the chatbot interface</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
